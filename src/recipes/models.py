7e02e36fe0a39ff0e38f97a56d7509eca6cf669c
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-06-30 12:47:43 +0200

    Update models.py
    increase type charfield length to 50
    calculate difficulty defined
    changed words in type_choices to start with capitals

--- src/recipes/models.py
+++ src/recipes/models.py
@@ -1,21 +1,42 @@
 from django.db import models
+from django.shortcuts import reverse
+
 type_choices =(
-  ('breakfast', 'Breakfast'),
-  ('drink', 'Drink'),
-  ('lunch', 'Lunch'),
-  ('snack','Snack'),
-  ('tea', 'Tea'),
+  ('Breakfast', 'Breakfast'),
+  ('Drink', 'Drink'),
+  ('Lunch', 'Lunch'),
+  ('Snack','Snack'),
+  ('Tea', 'Tea'),
+  ('Dinner', 'Dinner'),
 )
+
 # Create your models here.
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
     ingredients = models.CharField(max_length=255, help_text="You must use a comma to separate the ingredients.")
     cooking_time = models.PositiveIntegerField(help_text="In minutes")
-    type = models.CharField(max_length=20, choices=type_choices, default="br")
+    type = models.CharField(max_length=50, choices=type_choices)
     instructions = models.TextField()
+    pic = models.ImageField(upload_to='recipes', default='no_picture.jpg')
 
     def __str__(self):
       return str(self.name)
+
+    def calculate_difficulty(self):
+      ingredients = self.ingredients.split(', ')
+      if self.cooking_time < 10 and len(ingredients) < 5:
+        difficulty = "Easy"
+      elif self.cooking_time < 10 and len(ingredients) >= 5:
+        difficulty = "Medium"
+      elif self.cooking_time >= 10 and len(ingredients) < 5:
+        difficulty = "Intermediate"
+      elif self.cooking_time >= 10 and len(ingredients) >= 5:
+        difficulty = "Hard"
+      return difficulty
+
+    def get_absolute_url(self):
+      return reverse ('recipes:detail', kwargs={'pk': self.pk})
+
 
 
 
