886011c877d946058613628fd034ca928ea9a0a0
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-06-30 12:50:45 +0200

    Update urls.py
    path to the new views defined

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import home
+from .views import recipe_home
+from .views import RecipeListView
+from .views import RecipeDetailView
 
 app_name = 'recipes'
 
 urlpatterns = [
-  path('', home),
+  path('', recipe_home),
+  path('list/', RecipeListView.as_view(), name='list'),
+  path('list/<pk>', RecipeDetailView.as_view(), name='detail'),
 ]
