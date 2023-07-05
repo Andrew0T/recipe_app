f17283d5bbe24e76ee73772a5633212c357f0862
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-07-05 17:04:11 +0200

    Update views.py
    updated views to include forms. pandas and chart

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Recipe
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from .models import Recipe
+from .forms import RecipeSearchForm
+import pandas as pd
+from .utils import get_recipename_from_id, get_chart
 
 # Create your views here.
 def recipe_home(request):
@@ -14,3 +18,28 @@ class RecipeListView(LoginRequiredMixin, ListView):
 class RecipeDetailView(LoginRequiredMixin, DetailView):
   model = Recipe
   template_name = 'recipes/detail.html'
+
+@login_required
+def searches(request):
+	form = RecipeSearchForm(request.POST or None)
+	recipes_df=None
+	chart = None
+
+	if request.method =='POST':
+		name = request.POST.get('name')
+		chart_type = request.POST.get('chart_type')
+
+		qs = Recipe.objects.filter(name=name)		
+		if qs:
+			recipes_df=pd.DataFrame(qs.values())	
+			recipes_df['id']=recipes_df['id'].apply(get_recipename_from_id)
+			chart=get_chart(chart_type, recipes_df, labels=recipes_df['cooking_time'].values)			
+			recipes_df=recipes_df.to_html()
+
+	context={
+			'form': form,
+			'recipes_df': recipes_df,
+			'chart': chart
+			}
+
+	return render(request, 'recipes/searches.html', context)
