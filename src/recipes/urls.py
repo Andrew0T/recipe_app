fef7038e908de0c3ea1e5d28e2e14b9e159930d9
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-07-05 17:02:19 +0200

    Update urls.py
    updated connections

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import recipe_home, RecipeListView, RecipeDetailView
+from .views import recipe_home, RecipeListView, RecipeDetailView, searches
 
 app_name = 'recipes'
 
 urlpatterns = [
-  path('', recipe_home),
+  path('', recipe_home, name='home'),
   path('list/', RecipeListView.as_view(), name='list'),
   path('list/<pk>', RecipeDetailView.as_view(), name='detail'),
+  path('searches', searches, name='searches'),
 ]
