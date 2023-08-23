af9cad18b265f41746e37ceed30f1d8af97d9900
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-08-23 09:30:14 +0200

    Update views.py
    add and define create view
    rename searches to search view
    redefine search view

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -3,7 +3,7 @@ from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from .models import Recipe
-from .forms import RecipeSearchForm
+from .forms import RecipeSearchForm, CreateRecipeForm
 import pandas as pd
 from .utils import get_recipename_from_id, get_chart
 
@@ -20,26 +20,77 @@ class RecipeDetailView(LoginRequiredMixin, DetailView):
   template_name = 'recipes/detail.html'
 
 @login_required
-def searches(request):
-	form = RecipeSearchForm(request.POST or None)
-	recipes_df=None
+def search_view(request, *args, **kwargs):
+	search_form = RecipeSearchForm(request.POST or None)
+	recipes_df = None
 	chart = None
 
 	if request.method =='POST':
 		name = request.POST.get('name')
 		chart_type = request.POST.get('chart_type')
 
-		qs = Recipe.objects.filter(name=name)		
-		if qs:
+		qs = Recipe.objects.filter(name__contains=name)	
+		if len(qs) >0:
 			recipes_df=pd.DataFrame(qs.values())	
 			recipes_df['id']=recipes_df['id'].apply(get_recipename_from_id)
-			chart=get_chart(chart_type, recipes_df, labels=recipes_df['cooking_time'].values)			
+			chart=get_chart(chart_type, recipes_df, labels=recipes_df['name'].values)
+			recipes_df = recipes_df[['name']]
 			recipes_df=recipes_df.to_html()
+			recipes_df = recipes_df.replace('&lt;br /&gt;', '<br />').replace('\\r\\n', '')
+			for recipe in qs.values('name','id'):
+				recipe_name = recipe['name']
+				recipe_id = recipe['id']
+				recipes_df = recipes_df.replace(
+				f'<td>{recipe_name}</td>',
+				f'<td><a href="/list/{recipe_id}">{recipe_name}</a></td>')
+				
 
+			# breakpoint()
+				
 	context={
-			'form': form,
+			'search_form': search_form,
 			'recipes_df': recipes_df,
 			'chart': chart
-			}
+			}			
 
 	return render(request, 'recipes/searches.html', context)
+
+@ login_required
+def create_view(request):
+	create_recipe_form = CreateRecipeForm(request.POST, request.FILES)
+	name = None
+	ingredients = None
+	cooking_time = None
+	type = None
+	instructions = None
+	pic = None
+
+	if request.method == 'POST':
+
+		try:
+				recipe = Recipe.objects.create(
+						name = request.POST.get('name'),
+						ingredients = request.POST.get('ingredients'),
+						cooking_time = request.POST.get('cooking_time'),
+						type = request.POST.get('type'),
+						instructions= request.POST.get('instructions'),
+						pic = request.POST.get('pic')
+				)
+	
+				recipe.save()
+
+		except:
+				print('Error...sorry something went wrong!!!')
+
+	context = {
+			'create_recipe_form': create_recipe_form,
+			'name': name,
+			'ingredients': ingredients,
+			'cooking_time': cooking_time,
+			'type': type,
+			'instructions': instructions,
+			'pic': pic
+	}
+
+	return render(request, 'recipes/create.html', context)
+
