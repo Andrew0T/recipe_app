b1e57aa9d2ec725d0c3c205d78e2396d06a627f4
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-08-23 10:00:13 +0200

    Update utils.py
    re-aligning code to reduce page length

--- src/recipes/utils.py
+++ src/recipes/utils.py
@@ -11,29 +11,16 @@ def get_recipename_from_id(val):
     return recipename
 
 def get_graph():
-    #create a BytesIO buffer for the image
-    buffer = BytesIO()          
+    
+    buffer = BytesIO()                  #create a BytesIO buffer for the image
+    plt.savefig(buffer, format='png')   #create plot with bytesIO object as a file-like object. Set format to png
+    buffer.seek(0)                      #set cursor to the beginning of the stream
+    image_png=buffer.getvalue()         #retrieve the content of the file
+    graph=base64.b64encode(image_png)   #encode the bytes-like object
+    graph=graph.decode('utf-8')         #decode to get the string as output
+    buffer.close()                      #free up the memory of buffer
 
-    #create plot with bytesIO object as a file-like object. Set format to png
-    plt.savefig(buffer, format='png') 
-
-    #set cursor to the beginning of the stream
-    buffer.seek(0)
-
-    #retrieve the content of the file
-    image_png=buffer.getvalue()
-
-    #encode the bytes-like object
-    graph=base64.b64encode(image_png)
-
-    #decode to get the string as output
-    graph=graph.decode('utf-8')
-
-    #free up the memory of buffer
-    buffer.close()
-
-    #return the image/graph
-    return graph
+    return graph                        #return the image/graph
 
 #chart_type: user input o type of chart, 
 #data: pandas dataframe
@@ -41,28 +28,23 @@ def get_chart(chart_type, data, **kwargs):
     #switch plot backend to AGG (Anti-Grain Geometry) - to write to file
     #AGG is preferred solution to write PNG files
     plt.switch_backend('AGG')
+    fig=plt.figure(figsize=(6,3))       #specify figure size
 
-    #specify figure size
-    fig=plt.figure(figsize=(6,3))
-
-    #select chart_type based on user input from the form
-    if chart_type == '#1':
-        #plot bar chart between recipe name on x-axis and cooking_time on y-axis
-        plt.bar(data['name'], data['cooking_time'])
+    if chart_type == '#1':              #select chart_type based on user input from the form
+                                        #plot bar chart between recipe name on x-axis and cooking_time on y-axis
+        plt.bar(data['cooking_time'], data['name'])
 
     elif chart_type == '#2':
         labels=kwargs.get('labels')
         plt.pie(data['cooking_time'], labels=labels)
 
     elif chart_type == '#3':
-        #plot line chart based on recipe name on x-axis and meal type on y-axis
-        plt.plot(data['name'], data['cooking_time'])
+                                        #plot line chart based on recipe name on x-axis and meal type on y-axis
+        plt.plot(data['cooking_time'], data['name'])
     else:
         print ('unknown chart type')
     
-    #specify layout details
-    plt.tight_layout()
+    plt.tight_layout()                  #specify layout details
 
-    #render the graph to file
-    chart =get_graph()  
-    return chart        
+    chart =get_graph()                  #render the graph to file
+    return chart    
