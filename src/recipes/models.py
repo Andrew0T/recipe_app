15bb263a0cd83aac1a060091ee5ab84fc8c76582
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-08-23 09:35:23 +0200

    Update models.py
    increase CharField max-length values for name and ingredients

--- src/recipes/models.py
+++ src/recipes/models.py
@@ -12,8 +12,8 @@ type_choices =(
 
 # Create your models here.
 class Recipe(models.Model):
-    name = models.CharField(max_length=50)
-    ingredients = models.CharField(max_length=255, help_text="You must use a comma to separate the ingredients.")
+    name = models.CharField(max_length=120)
+    ingredients = models.CharField(max_length=300, help_text="You must use a comma to separate the ingredients.")
     cooking_time = models.PositiveIntegerField(help_text="In minutes")
     type = models.CharField(max_length=50, choices=type_choices)
     instructions = models.TextField()
