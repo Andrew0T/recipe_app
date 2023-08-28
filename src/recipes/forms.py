ef4f3b37242a7ede147aa966882bb1855fa77d51
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-08-28 08:02:02 +0200

    Update forms.py
    create recipe and edit recipe forms defined

--- src/recipes/forms.py
+++ src/recipes/forms.py
@@ -1,4 +1,9 @@
 from django import forms	#import django forms
+<<<<<<< Updated upstream
+=======
+from django.forms import ModelForm
+from .models import Recipe, TYPE_CHOICES
+>>>>>>> Stashed changes
 
 CHART__CHOICES = (			#specify choices as a tuple
 	('#1', 'Bar chart'),	# when user selects "Bar chart", it is stored as "#1"
@@ -8,5 +13,45 @@ CHART__CHOICES = (			#specify choices as a tuple
 
 #define class-based Form imported from Django forms
 class RecipeSearchForm(forms.Form):	
+<<<<<<< Updated upstream
     name= forms.CharField(max_length=120)
     chart_type = forms.ChoiceField(choices=CHART__CHOICES)
+=======
+	name= forms.CharField(max_length=120)
+	chart_type = forms.ChoiceField(choices=CHART__CHOICES)
+
+# class CreateRecipeForm(ModelForm):
+#   class Meta:
+#     model = Recipe
+#     fields = ('name','ingredients','cooking_time','type','instructions','pic')
+
+class CreateRecipeForm(forms.Form):
+	name = forms.CharField(max_length=120)
+	ingredients = forms.CharField(max_length=300, help_text="You must use a comma to separate the ingredients.")
+	cooking_time = forms.IntegerField(help_text="In minutes")
+	type = forms.ChoiceField(choices=TYPE_CHOICES)
+	instructions = forms.CharField(max_length=600)
+	pic = forms.ImageField()
+
+
+class EditRecipeForm(forms.Form):
+	name = forms.CharField(max_length=120)
+	ingredients = forms.CharField(max_length=300, help_text="You must use a comma to separate the ingredients.")
+	cooking_time = forms.IntegerField(help_text="In minutes")
+	type = forms.ChoiceField(choices=TYPE_CHOICES)
+	instructions = forms.CharField(max_length=600)
+	pic = forms.ImageField()
+
+
+# class EditRecipeForm(forms.ModelForm):
+#   class Meta:
+#     model = Recipe
+#     fields = [
+# 						'name',
+# 						'ingredients',
+# 						'cooking_time',
+# 						'type',
+# 						'instructions',
+# 						'pic',
+# 							]
+>>>>>>> Stashed changes
