a677cd982ab809bcf8f7c8b0621a1b24dfa31408
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-08-28 07:59:44 +0200

    Update views.py
    create and edit recipe views defined and forms imported

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -3,7 +3,11 @@ from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from .models import Recipe
+<<<<<<< Updated upstream
 from .forms import RecipeSearchForm
+=======
+from .forms import RecipeSearchForm, CreateRecipeForm, EditRecipeForm
+>>>>>>> Stashed changes
 import pandas as pd
 from .utils import get_recipename_from_id, get_chart
 
@@ -40,6 +44,96 @@ def searches(request):
 			'form': form,
 			'recipes_df': recipes_df,
 			'chart': chart
+<<<<<<< Updated upstream
 			}
 
 	return render(request, 'recipes/searches.html', context)
+=======
+			}			
+
+	return render(request, 'recipes/searches.html', context)
+
+@ login_required
+def create_view(request):
+	form = CreateRecipeForm(request.POST, request.FILES)
+	create_recipe_form = form
+	name = None
+	ingredients = None
+	cooking_time = None
+	type = None
+	instructions = None
+	pic = None
+
+	if request.method == 'POST':
+
+		if form.is_valid():
+				name = form.cleaned_data.get('name'),
+				pic = form.cleaned_data.get('pic')
+				recipe = Recipe.objects.create(
+						name = request.POST.get('name'),
+						ingredients = request.POST.get('ingredients'),
+						cooking_time = request.POST.get('cooking_time'),
+						type = request.POST.get('type'),
+						instructions= request.POST.get('instructions'),
+						pic = request.POST.get('pic')
+				)
+	
+				recipe.save()
+
+		else:
+				print('Error...sorry something went wrong!!!')
+
+	context = {
+			'create_recipe_form': create_recipe_form,
+			'name': name,
+			'ingredients': ingredients,
+			'cooking_time': cooking_time,
+			'type': type,
+			'instructions': instructions,
+			'pic': pic,}
+
+	return render(request, 'recipes/create.html', context)
+
+@ login_required
+def edit_view(request):
+	form = EditRecipeForm(request.POST, request.FILES)
+	edit_recipe_form = form
+	name = None
+	ingredients = None
+	cooking_time = None
+	type = None
+	instructions = None
+	pic = None
+
+	if request.method == 'POST':
+
+		if form.is_valid():
+				name = form.cleaned_data.get('name'),
+				pic = form.cleaned_data.get('pic')
+				recipe = Recipe.objects.all(
+						name = request.POST.get('name'),
+						ingredients = request.POST.get('ingredients'),
+						cooking_time = request.POST.get('cooking_time'),
+						type = request.POST.get('type'),
+						instructions= request.POST.get('instructions'),
+						pic = request.POST.get('pic')
+				)
+	
+				recipe.save()
+
+		else:
+				print('Error...sorry something went wrong!!!')
+
+	context = {
+			'edit_recipe_form': edit_recipe_form,
+			'name': name,
+			'ingredients': ingredients,
+			'cooking_time': cooking_time,
+			'type': type,
+			'instructions': instructions,
+			'pic': pic,
+	}
+
+	return render(request, 'recipes/edit_recipe.html', context)
+
+>>>>>>> Stashed changes
