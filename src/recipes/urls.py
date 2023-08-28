e77f37cf83af8a58ab8c4d23d053042e3de4b428
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-08-28 07:58:01 +0200

    Update urls.py
    create, search and edit_recipe views and paths defined

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+<<<<<<< Updated upstream
 from .views import recipe_home, RecipeListView, RecipeDetailView, searches
+=======
+from .views import recipe_home, RecipeListView, RecipeDetailView, search_view, create_view, edit_view
+>>>>>>> Stashed changes
 
 app_name = 'recipes'
 
@@ -7,5 +11,12 @@ urlpatterns = [
   path('', recipe_home, name='home'),
   path('list/', RecipeListView.as_view(), name='list'),
   path('list/<pk>', RecipeDetailView.as_view(), name='detail'),
+<<<<<<< Updated upstream
   path('searches', searches, name='searches'),
+=======
+  path('searches', search_view, name='searches'),
+  path('create', create_view, name='create'),
+  path('edit_recipe', edit_view, name='edit_recipe'),
+
+>>>>>>> Stashed changes
 ]
