c1823eeeff52699b94223f091231478dc98399e1
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-08-29 14:04:35 +0200

    Update forms.py
    ModelForm used instead of Form

--- src/recipes/forms.py
+++ src/recipes/forms.py
@@ -13,37 +13,14 @@ class RecipeSearchForm(forms.Form):
 	name= forms.CharField(max_length=120)
 	chart_type = forms.ChoiceField(choices=CHART__CHOICES)
 
-# class CreateRecipeForm(ModelForm):
-#   class Meta:
-#     model = Recipe
-#     fields = ('name','ingredients','cooking_time','type','instructions','pic')
-
-class CreateRecipeForm(forms.Form):
-	name = forms.CharField(max_length=120)
-	ingredients = forms.CharField(max_length=300, help_text="You must use a comma to separate the ingredients.")
-	cooking_time = forms.IntegerField(help_text="In minutes")
-	type = forms.ChoiceField(choices=TYPE_CHOICES)
-	instructions = forms.CharField(max_length=600)
-	pic = forms.ImageField()
+class CreateRecipeForm(ModelForm):
+  class Meta:
+    model = Recipe
+    fields = ('name','ingredients','cooking_time','type','instructions','pic')
 
 
-class EditRecipeForm(forms.Form):
-	name = forms.CharField(max_length=120)
-	ingredients = forms.CharField(max_length=300, help_text="You must use a comma to separate the ingredients.")
-	cooking_time = forms.IntegerField(help_text="In minutes")
-	type = forms.ChoiceField(choices=TYPE_CHOICES)
-	instructions = forms.CharField(max_length=600)
-	pic = forms.ImageField()
+class EditRecipeForm(ModelForm):
+  class Meta:
+    model = Recipe
+    fields = ('name','ingredients','cooking_time','type','instructions','pic')
 
-
-# class EditRecipeForm(forms.ModelForm):
-#   class Meta:
-#     model = Recipe
-#     fields = [
-# 						'name',
-# 						'ingredients',
-# 						'cooking_time',
-# 						'type',
-# 						'instructions',
-# 						'pic',
-# 							]
